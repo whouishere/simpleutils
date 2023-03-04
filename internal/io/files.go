d50fe99914335e2334df8c88746244e903d51162
whouishere <willianvinagre11@gmail.com>
2023-03-04 07:41:20 -0300

    `FileIsDir` and recursive `IsDirEmpty`

--- internal/io/files.go
+++ internal/io/files.go
@@ -4,6 +4,7 @@ import (
 	"os"
 )
 
+// Returns true if a file exists
 func FileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err != nil {
@@ -15,4 +16,37 @@ func FileExists(path string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+// Returns true if a given path is a directory
+func FileIsDir(path string) (bool, error) {
+	file, err := os.Stat(path)
+	return file.IsDir(), err
+}
+
+// Recursively checks if a directory does not have any files.
+func IsDirEmpty(path string) (bool, error) {
+	dir, err := os.ReadDir(path)
+
+	// if dir is not empty
+	if len(dir) != 0 {
+		// check if there are anything other than folders
+		for _, dirfile := range dir {
+			// if any file returns false to IsDir(), return false
+			if !dirfile.IsDir() {
+				return false, err
+			}
+
+			// check if inner dir has something
+			innerdirisempty, err := IsDirEmpty(dirfile.Name())
+			if !innerdirisempty {
+				return false, err
+			}
+		}
+
+		// return true when looped through empty dirs
+		return true, err
+	}
+
+	return true, err
 }
